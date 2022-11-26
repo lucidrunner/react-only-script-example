613efcf38f32f2581285a28efd1f785cb1606076
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-26 02:30:08 +0100

    Created an example of local storage array storage
    tldr we can use this for the booking page to "create" a database by getting the array from the localstorage, append our new item to it, save it again with the same key and then loading it on the other page

--- components/App.js
+++ components/App.js
@@ -5,11 +5,62 @@ const App = () => {
     fontSize: "20pt",
   };
 
+  const saveArray = () => {
+    window.localStorage.setItem("myArray", JSON.stringify(loadedArray));
+    alert("saved");
+    loadedArray = [];
+  };
+
+  const loadArray = () => {
+    alert("loaded");
+    loadedArray = JSON.parse(window.localStorage.getItem("myArray"));
+  };
+
+  const appendArray = () => {
+    dbArray.map((item) => {
+      loadedArray.push(item);
+    });
+  };
+
+  const printArray = () => {
+    console.log(loadedArray);
+  };
+
+  const clearArray = () => {
+    alert("cleared");
+    loadedArray = [];
+  };
+
   return (
     <div>
       <CounterButton text="Hi!" />
       <p style={pStyle}>Hello World!</p>
       <CounterButton useGreen="1" />
+      <button onClick={saveArray}>Save Array</button>
+      <button onClick={loadArray}>Load Array</button>
+      <button onClick={appendArray}>appendArray Array</button>
+      <button onClick={printArray}>print Array</button>
+      <button onClick={clearArray}>clear Array</button>
     </div>
   );
 };
+
+let loadedArray = [];
+
+const dbArray = [
+  "First level item",
+  {
+    name: "first level object",
+    type: "first level object type",
+  },
+  {
+    name: "second first level object",
+    info: {
+      deepinfo: "object in the second first level object",
+    },
+  },
+  {
+    name: "third first level object",
+    type: "third first level object type",
+  },
+];
